0d31e867f1cc50221acdaf1784f296afc7fb7a08
Jens Stein Jørgensen <jsj@dbc.dk>
2018-06-11 15:32:49 +0200

    Change application path from / to /api
    Otherwise the index.html file (or any other welcome file) will not load
    when the application root is requested.

--- src/main/java/dk/dbc/saturn/rest/Saturn.java
+++ src/main/java/dk/dbc/saturn/rest/Saturn.java
@@ -17,7 +17,8 @@ import java.util.Set;
  * This class defines the other classes that make up this JAX-RS application by
  * having the getClasses method return a specific set of resources.
  */
-@ApplicationPath("/")
+// application path must not be "/" if webapp/index.html is to be loaded
+@ApplicationPath("/api")
 public class Saturn extends Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(
         dk.dbc.saturn.rest.Saturn.class);
