023515e6de0d784cf4b52b0529edd8f8cc705a7a
Jens Stein Jørgensen <jsj@dbc.dk>
2018-06-20 14:40:55 +0200

    Add component for editing ftp harvester configs

---
+++ gui/app/components/FtpHarvesterConfigEdit.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+import React from "react";
+
+import {BaseHarvesterConfigEdit, FormEntry} from "./BaseHarvesterConfigEdit";
+import FtpHarvesterConfig from "../model/FtpHarvesterConfig";
+
+class FtpHarvesterConfigEdit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {"config": {}};
+        this.fetchConfig = this.fetchConfig.bind(this);
+        this.onSave = this.onSave.bind(this);
+        this.onConfigChanged = this.onConfigChanged.bind(this);
+        this.onChangeCallback = this.onChangeCallback.bind(this);
+    }
+    fetchConfig(id) {
+        FtpHarvesterConfig.fetchConfig(id).end().then(response => {
+            const config = JSON.parse(response.text);
+            this.setState({config});
+        });
+    }
+    onConfigChanged(config) {
+        this.setState({config});
+    }
+    onChangeCallback(name, value) {
+        const config = this.state.config;
+        config[name] = value;
+        this.setState({config});
+    }
+    onSave(form) {
+        const config = new FtpHarvesterConfig();
+        // if id is undefined, a new entity will be created in the database
+        // by the backend
+        config.id = this.state.config.id;
+        for(let i = 0; i < form.length; i++) {
+            if(form[i] === undefined) continue;
+            switch(form[i].name) {
+            case "host":
+                config.host = form[i].value;
+                break;
+            case "port":
+                config.port = Number.parseInt(form[i].value);
+                break;
+            case "username":
+                config.username = form[i].value;
+                break;
+            case "password":
+                config.password = form[i].value;
+                break;
+            case "dir":
+                config.dir = form[i].value;
+                break;
+            case "filesPattern":
+                config.filesPattern = form[i].value;
+                break;
+            case "name":
+                config.name = form[i].value;
+                break;
+            case "schedule":
+                config.schedule = form[i].value;
+                break;
+            case "transfile":
+                config.transfile = form[i].value;
+                break;
+            default:
+                break;
+            }
+        }
+        FtpHarvesterConfig.addFtpHarvesterConfig(config).end()
+            .catch(err => console.error("unexpected error when adding config",
+            config, err));
+    }
+    componentWillMount() {
+        this.fetchConfig(this.props.match.params.id);
+    }
+    render() {
+        return (
+            <BaseHarvesterConfigEdit config={this.state.config}
+                    onSave={this.onSave}
+                    onConfigChanged={this.onConfigChanged}>
+                <FormEntry name="host" value={this.state.config.host}
+                    onChangeCallback={this.onChangeCallback}/>
+                <FormEntry name="port" value={
+                    this.state.config.port !== undefined ?
+                        this.state.config.port.toString() : "0"}
+                    onChangeCallback={this.onChangeCallback}/>
+                <FormEntry name="username" value={this.state.config.username}
+                    onChangeCallback={this.onChangeCallback}/>
+                <FormEntry name="password" value={this.state.config.password}
+                    onChangeCallback={this.onChangeCallback}/>
+                <FormEntry name="dir" value={this.state.config.dir}
+                    onChangeCallback={this.onChangeCallback}/>
+                <FormEntry name="filesPattern" value={this.state.config.filesPattern}
+                    onChangeCallback={this.onChangeCallback}/>
+            </BaseHarvesterConfigEdit>
+        )
+    }
+}
+
+export default FtpHarvesterConfigEdit;
