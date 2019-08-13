5c088d9c0cb0e1981eb758ea5bf6419561deb240
Jesper Bruun <jbr@dbc.dk>
2019-08-13 15:52:09 +0200

    Use write lock
    And settle for just holding the config ids as state info

--- api/src/main/java/dk/dbc/saturn/RunningTasks.java
+++ api/src/main/java/dk/dbc/saturn/RunningTasks.java
@@ -4,38 +4,40 @@ import dk.dbc.saturn.entity.FtpHarvesterConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.ejb.Singleton;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Singleton
+@Lock(LockType.WRITE)
 public class RunningTasks {
-    private final static HashMap<Integer, Set<FileHarvest>> runningHarvestTasks = new HashMap<>();
+    private final Set<Integer> runningHarvestTasks = new HashSet<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(
             RunningTasks.class);
 
     public boolean isRunning( FtpHarvesterConfig config ) {
 
-        return runningHarvestTasks.containsKey( config.getId() );
+        return runningHarvestTasks.contains( config.getId() );
     }
 
-    public void remove( FtpHarvesterConfig config ) {
+    public  void remove( FtpHarvesterConfig config ) {
         if (isRunning(config) ) {
             runningHarvestTasks.remove( config.getId() );
         }
         LOGGER.info( "Removed task {}. NOW the list of running tasks is:{}",
                 config.getId(),
-                String.join(",",runningHarvestTasks.keySet().stream()
-                        .map(String::valueOf)
-                        .collect(Collectors.toList())));
+                String.join(",",runningHarvestTasks.stream()
+                        .map(String::valueOf).collect(Collectors.toList())));
     }
 
     public void add( FtpHarvesterConfig config, Set<FileHarvest> fileHarvests ) throws HarvestException {
         if ( isRunning( config) ) {
             throw new HarvestException( String.format("%s already running.", config.getName()));
         }
-        runningHarvestTasks.put( config.getId(), fileHarvests );
+        runningHarvestTasks.add( config.getId() );
     }
 
     public int size() { return runningHarvestTasks.size(); }
