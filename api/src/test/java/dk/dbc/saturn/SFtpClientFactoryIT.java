fe367aa3637c31bad2c56fcce969adf8d3ce560b
Jesper Bruun <jbr@dbc.dk>
2020-07-24 15:11:59 +0200

    Fix WIP temporals
    Auditors: atm, jbn, hwha, mib

--- api/src/test/java/dk/dbc/saturn/SFtpClientFactoryIT.java
+++ api/src/test/java/dk/dbc/saturn/SFtpClientFactoryIT.java
@@ -1,9 +1,11 @@
 package dk.dbc.saturn;
 
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.SftpException;
+import dk.dbc.saturn.sftp.client.SFtpClient;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +15,26 @@ class SFtpClientFactoryIT extends AbstractIntegrationTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SFtpClientFactoryIT.class);
 
     @Test
-    public void test_simpleTest() throws InterruptedException, SftpException, JSchException, IOException {
-        LOGGER.info("host: {}, user: {}, dir:{}", SFTP_ADDRESS, SFTP_USER, SFTP_DIR);
-        ChannelSftp channelSftp = SFtpClientFactory.createSFtpClient(SFTP_ADDRESS, SFTP_PORT, SFTP_USER, SFTP_PASSWORD, SFTP_DIR, null);
-        LOGGER.info("LS:{}",channelSftp.ls("*"));
+    public void test_simpleTest() throws IOException {
+        try (SFtpClient sFtpClient = SFtpClient.getClient(SFTP_ADDRESS, SFTP_USER, SFTP_PASSWORD, SFTP_PORT, "upload", null)) {
+            LOGGER.info("host: {}, user: {}, dir:{}", SFTP_ADDRESS, SFTP_USER, SFTP_DIR);
+            sFtpClient.putContent("hej.txt", new ByteArrayInputStream("Hej".getBytes()));
+            LOGGER.info("LS:{}", sFtpClient.ls("*"));
+            LOGGER.info("pwd:{}", sFtpClient.pwd());
+            LOGGER.info("Filecontent is:{}", readInputStream(sFtpClient.getContent("hej.txt")));
+        }
+    }
+
+    protected static String readInputStream(InputStream is) throws IOException {
+        try (final BufferedReader in = new BufferedReader(
+                new InputStreamReader(is))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString().trim();
+        }
     }
 
 }
